aa73f7c126b73d7ec70bc7cf2768db9c02c7b713
wyvr <wyvr@daviot.at>
2021-07-01 07:08:52 +0200

    :memo: add description to commands

--- src/question.js
+++ src/question.js
@@ -12,19 +12,19 @@ module.exports = {
                     default: 'component',
                     choices: [
                         {
-                            name: '📦️ Component',
+                            name: `📦️ Component ${colors.dim('generate a new component')}`,
                             value: 'component',
                         },
                         {
-                            name: '🎨 Theme',
+                            name: `🎨 Theme ${colors.dim('scaffolds a new theme')}`,
                             value: 'theme',
                         },
                         {
-                            name: '⚡️ Plugin',
+                            name: `⚡️ Plugin ${colors.dim('scaffolds a new plugin')}`,
                             value: 'plugin',
                         },
                         {
-                            name: '🚀 Project',
+                            name: `🚀 Project ${colors.dim('scaffolds a new project')}`,
                             value: 'project',
                         },
                     ],
@@ -82,19 +82,19 @@ module.exports = {
                     default: 'component',
                     choices: [
                         {
-                            name: '📦️ Component',
+                            name: `📦️ Component ${colors.dim('generate a component')}`,
                             value: 'component',
                         },
                         {
-                            name: '📄 Page',
+                            name: `📄 Page ${colors.dim('generate a page component')}`,
                             value: 'page',
                         },
                         {
-                            name: '🍱 Layout',
+                            name: `🍱 Layout ${colors.dim('generate a layout component')}`,
                             value: 'layout',
                         },
                         {
-                            name: '🔧 Document',
+                            name: `🔧 Document ${colors.dim('generate a doc component')}`,
                             value: 'doc',
                         },
                     ],
@@ -113,19 +113,19 @@ module.exports = {
         const predefined = ['hydrate', 'on_server'];
         const choices = [
             {
-                name: '⚡️ onServer',
+                name: `⚡️ onServer ${colors.dim('adds the onServer hook to the component')}`,
                 value: 'on_server',
             },
             {
-                name: '✨ Hydrate',
+                name: `✨ Hydrate ${colors.dim('makes the component hydrateable')}`,
                 value: 'hydrate',
             },
             {
-                name: '🙈 Lazy Loading',
+                name: `🙈 Lazy Loading ${colors.dim('makes the component lazy loaded, when in view')}`,
                 value: 'lazy',
             },
             {
-                name: '🚚 Portal',
+                name: `🚚 Portal ${colors.dim('allows the component to be portaled to another node')}`,
                 value: 'portal',
             },
         ];
@@ -140,7 +140,7 @@ module.exports = {
                     choices,
                 },
             ]);
-            // when lazy or portal hydrate is required
+            // when lazy or portal, hydrate is required
             if (result.indexOf('hydrate') == -1 && (result.indexOf('lazy') > -1 || result.indexOf('portal') > -1)) {
                 return ['hydrate', ...result];
             }
