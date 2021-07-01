e1d90b601dc8fdf79965e47783c8b8c1aa9a5da4
wyvr <wyvr@daviot.at>
2021-07-01 06:33:12 +0200

    :sparkles: add correct dir for components

--- src/helper.js
+++ src/helper.js
@@ -1,13 +1,43 @@
 const { readdirSync, existsSync } = require('fs');
 const { join, extname, basename } = require('path');
 const helper = {
-
-    find_cwd(type) {
+    correct_dir(type, flavour) {
         const cwd = process.cwd();
-        // let required_parts = [];
-        // if (['component'].indexOf(type) > -1) {
-        //     required_parts.push('/src/');
-        // }
+        let required_parts = [];
+        if (['doc', 'page', 'layout', 'component'].indexOf(type) > -1) {
+            required_parts.push(join('src'));
+        }
+        if (['doc'].indexOf(type) > -1) {
+            required_parts.push(join('src', 'doc'));
+        }
+        if (['layout'].indexOf(type) > -1) {
+            required_parts.push(join('src', 'layout'));
+        }
+        if (['page'].indexOf(type) > -1) {
+            required_parts.push(join('src', 'page'));
+        }
+        const found_parts = [];
+        const missing_parts = [];
+        required_parts.forEach((part) => {
+            if (cwd.indexOf(part) > -1) {
+                found_parts.push(part);
+                return;
+            }
+            missing_parts.push(part);
+        });
+        if (found_parts.length == required_parts.length) {
+            return cwd;
+        }
+        if (missing_parts.length > 0) {
+            const corrected_dir = missing_parts.filter((part) => existsSync(join(cwd, part)));
+            if (corrected_dir.length > 0) {
+                return corrected_dir
+                    .reverse()
+                    .map((part) => join(cwd, part))
+                    .find((x) => x);
+            }
+        }
+
         return cwd;
     },
     contains_folder_wyvr_file(dir) {
